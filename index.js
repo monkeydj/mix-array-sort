e84680fb03a87f3a3daad0efdedefa51e143419b
duy.ton <duy.ton@exe.com.vn>
2019-09-19 17:10:35 +0700

    add some test cases & minor updates

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const sort = require('./sort');
 var tests = require('./test-cases');
 
 for (let { input, output } of tests) {
-    assert.deepStrictEqual(sort(input), output);
+    let actual = sort(input);
+    console.log(`testing with input: ${JSON.stringify(input)}`);
+    console.log(`expected output: ${JSON.stringify(output)}`);
+    console.log(`actual result: ${JSON.stringify(actual)}`);
+    assert.deepStrictEqual(actual, output);
 }
