3c014d6d2e5c1aab5479c265816e2c8084b4e450
duy.ton <duy.ton@exe.com.vn>
2019-09-19 17:14:42 +0700

    minor fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var tests = require('./test-cases');
 
 for (let { input, output } of tests) {
     let actual = sort(input);
+    console.log('-'.repeat(22)); // separation between test cases
     console.log(`testing with input: ${JSON.stringify(input)}`);
     console.log(`expected output: ${JSON.stringify(output)}`);
     console.log(`actual result: ${JSON.stringify(actual)}`);
